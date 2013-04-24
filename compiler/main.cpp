ab02ea608d6652f5ce2bf885f938eb92d237380a
Nate Grandner <ngrandner@gmail.com>
2013-04-24 13:17:28 -0400

    Updated compiler and syntax

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 enum token_t
 {
@@ -60,49 +61,205 @@ enum word_offset_t
 	CTRL_OFF = 30
 };
 
+static int lineno;
+static char* filename;
+static char* lexer_ptr;
+
 int print_usage(char* filename)
 {
 	fprintf(stderr,"usage: %s [-o outfile] file\n",filename);
 	return -1;
 }
-
-int next_token(int* value, char* buffer, char* line, char** outline)
+int error(const char* format,...)
 {
-	*outline = line;
-	*buffer = '\0';
-	while (isspace(*line)) ++line;
-	if (*line == '\0')
+	va_list list;
+	va_start(list,format);
+	fprintf(stderr,"%s:%d: error: ",filename,lineno);
+	vfprintf(stderr,format,list);
+	fputc('\n',stderr);
+	va_end(list);
+	return -1;
+}
+
+int next_token(int* value, char* buffer)
+{
+	if (buffer) *buffer = '\0';
+	while (isspace(*lexer_ptr)) ++lexer_ptr;
+	if (*lexer_ptr == '\0')
 		return NONE;
-	if (isdigit(*line))
+	if (isdigit(*lexer_ptr))
 	{
-		value = strtoul(line,outline,0);
+		int v = strtoul(lexer_ptr,&lexer_ptr,0);
+		if (value)
+			*value = v;
 		return INT;
 	}
-	if (isalpha(*line) || *line == '_' || *line == '.')
+	if (isalpha(*lexer_ptr) || *lexer_ptr == '_')
 	{
 		do
 		{
-			*buffer++ = *line++;
-		} while (isalnum(*line) || *line == '_';
-		*buffer = '\0';
-		*outline = line;
+			if (buffer) *buffer++ = *lexer_ptr;
+			++lexer_ptr;
+		} while (isalnum(*lexer_ptr) || *lexer_ptr == '_');
+		if (buffer) *buffer = '\0';
 		return WORD;
 	}
-	*buffer++ = *line++;
-	*buffer = '\0';
-	*outline = line;
+	if (buffer)
+	{
+		*buffer++ = *lexer_ptr;
+		*buffer = '\0';
+	}
+	++lexer_ptr;
 	return PUNC;
+}
+int expect(const char* exp, char* buffer)
+{
+	if (strcmp(exp,buffer))
+		return error("Expected: '%s', got '%s'",exp,*buffer ? buffer : "<EOL>");
+	return 0;
+}
+void write_instr(FILE* out, unsigned int instr)
+{
+	// Because of endianness, we print out byte-by-byte
+	fputc((instr>>0)&0xFF,out);
+	fputc((instr>>8)&0xFF,out);
+	fputc((instr>>16)&0xFF,out);
+	fputc((instr>>24)&0xFF,out);
+}
+
+int parse_instr(FILE* out)
+{
+	unsigned int instr = 0;
+	int ctrl = NORMAL;
+	int val;
+	char lex[32];
+	if (next_token(NULL,lex) == NONE)
+		return 0;
+	if (!strcmp(lex,"LOAD"))
+		ctrl = LOAD, instr |= (OP_LD << ALU_OFF) | (1 << NEWS_OFF) | (IN_WEST << INSEL_OFF);
+	else if (!strcmp(lex,"SAVE"))
+		ctrl = SAVE, instr |= (OP_LD << ALU_OFF) | (1 << NEWS_OFF) | (IN_WEST << INSEL_OFF);
+	else if (!strcmp(lex,"CPY"))
+		instr |= OP_LD << ALU_OFF; // No change really, but for consistency sake
+	else if (!strcmp(lex,"AND"))
+		instr |= (OP_AND << ALU_OFF);
+	else if (!strcmp(lex,"IAND"))
+		instr |= (OP_AND << ALU_OFF) | (1 << INVACC_OFF);
+	else if (!strcmp(lex,"OR"))
+		instr |= (OP_OR << ALU_OFF);
+	else if (!strcmp(lex,"IOR"))
+		instr |= (OP_OR << ALU_OFF) | (1 << INVACC_OFF);
+	else if (!strcmp(lex,"XOR"))
+		instr |= (OP_XOR << ALU_OFF);
+	else if (!strcmp(lex,"IXOR"))
+		instr |= (OP_XOR << ALU_OFF) | (1 << INVACC_OFF);
+	else if (!strcmp(lex,"XOR"))
+		instr |= (OP_XOR << ALU_OFF);
+	else if (!strcmp(lex,"IXOR"))
+		instr |= (OP_XOR << ALU_OFF) | (1 << INVACC_OFF);
+	else if (!strcmp(lex,"SUM"))
+		instr |= (OP_SUM << ALU_OFF) | (1 << CLRCAR_OFF);
+	else if (!strcmp(lex,"ISUM"))
+		instr |= (OP_SUM << ALU_OFF) | (1 << INVACC_OFF) | (1 << CLRCAR_OFF);
+	else if (!strcmp(lex,"SUMC"))
+		instr |= (OP_SUM << ALU_OFF);
+	else if (!strcmp(lex,"ISUMCI"))
+		instr |= (OP_SUM << ALU_OFF) | (1 << INVACC_OFF);
+	else if (!strcmp(lex,"SET"))
+		instr |= (OP_SET << ALU_OFF);
+	else if (!strcmp(lex,"CLR"))
+		instr |= (OP_CLR << ALU_OFF);
+	else if (!strcmp(lex,"CAR"))
+		instr |= (OP_CAR << ALU_OFF) | (1 << CLRCAR_OFF);
+	else
+		return error("Unknown operation: '%s'",lex);
+	next_token(NULL,lex);
+	if (!strcmp(lex,"."))
+	{
+		next_token(NULL,lex);
+		if (!strcmp(lex,"INV"))
+			instr |= 1 << INVOUT_OFF;
+		else
+			return error("Unknown op extension: '%s'",lex);
+		next_token(NULL,lex);
+	}
+	if (expect("(",lex))
+		return -1;
+	while (next_token(NULL,lex) == WORD)
+	{
+		if (!strcmp(lex,"RAM"))
+		{
+			if (next_token(NULL,lex) != PUNC || expect("[",lex)) return -1;
+			if (next_token(&val,lex) != INT)
+				return error("Expected: integer");
+			instr |= 1 << RAM_OFF;
+			instr |= ((val & 0xFF) << IMGADDR_OFF);
+			if (next_token(NULL,lex) != PUNC || expect("]",lex)) return -1;
+		}
+		else if (!strcmp(lex,"FLAG")) // Flag assignment
+			instr |= 1 << FLAG_OFF;
+		else if (!strcmp(lex,"NEWS")) // NEWS assignment
+			instr |= 1 << NEWS_OFF;
+		else if (!strcmp(lex,"X")) // Etc...
+			instr |= 1 << GPREG_OFF;
+		else if (!strcmp(lex,"Y"))
+			instr |= 2 << GPREG_OFF;
+		else if (!strcmp(lex,"Z"))
+			instr |= 3 << GPREG_OFF;
+		else
+			return error("Unknown destination: '%s'",lex);
+	}
+	if (*lex == ',')
+	{
+		next_token(&val,lex);
+		if (ctrl == LOAD || ctrl == SAVE)
+			instr |= ((val & 0xFF) << IMGADDR_OFF);
+		else
+		{
+			if (!strcmp(lex,"NORTH"))
+				instr |= (IN_NORTH << INSEL_OFF);
+			else if (!strcmp(lex,"EAST"))
+				instr |= (IN_EAST << INSEL_OFF);
+			else if (!strcmp(lex,"SOUTH"))
+				instr |= (IN_SOUTH << INSEL_OFF);
+			else if (!strcmp(lex,"WEST"))
+				instr |= (IN_WEST << INSEL_OFF);
+			else if (!strcmp(lex,"X"))
+				instr |= (IN_X << INSEL_OFF);
+			else if (!strcmp(lex,"Y"))
+				instr |= (IN_Y << INSEL_OFF);
+			else if (!strcmp(lex,"Z"))
+				instr |= (IN_Z << INSEL_OFF);
+			else if (!strcmp(lex,"RAM"))
+			{
+				if (next_token(NULL,lex) != PUNC || expect("[",lex)) return -1;
+				if (next_token(&val,lex) != INT)
+					return error("Expected: integer");
+				instr |= (IN_RAM << INSEL_OFF);
+				instr |= ((val & 0xFF) << IMGADDR_OFF);
+				if (next_token(NULL,lex) != PUNC || expect("]",lex)) return -1;
+		
+			}
+			else
+				return error("Unknown operand: '%s'",lex);
+		}
+		next_token(NULL,lex);
+	}
+	if (expect(")",lex))
+		return -1;
+	
+	instr |= ctrl << CTRL_OFF;
+	write_instr(out,instr);
+	return 0;
 }
 
 int main (int argc, char* argv[])
 {
 	unsigned int instr;
 	char* arg;
-	char* infile;
 	const char* outfile = "a.out";
 	char buffer[512];
 	char lex[32];
-	int line = 0;
 	while (arg = *++argv)
 	{
 		if (*arg == '-')
@@ -121,30 +278,25 @@ int main (int argc, char* argv[])
 				return -1;
 			}
 		}
-		infile = arg;
+		filename = arg;
 	}
-	if (!infile)
+	if (!filename)
 		return print_usage(argv[0]);
-	FILE* in = fopen(infile,"r");
+	FILE* in = fopen(filename,"r");
 	if (!in)
 	{
-		fprintf(stderr,"Unable to open input file '%s'\n",infile);
+		fprintf(stderr,"Unable to open input file '%s'\n",filename);
 		return -1;
 	}
 	FILE* out = fopen(outfile,"wb");
 	if (!out)
 	{
-		fprintf(stderr,"Unable to open output file '%s'",outfile);
+		fprintf(stderr,"Unable to open output file '%s'",filename);
 		return -1;
 	}
 	while (fgets(buffer,512,in))
 	{
-		int ctrl = NORMAL;
 		int token;
-		char* param;
-		char* peq;
-		char* paramend;
-		int paramreq = 1;
 		char* ptr = strchr(buffer,'\n');
 		if (ptr) *ptr = '\0'; // Strip '\n'
 		ptr = strchr(buffer,'#'); // Strip comments
@@ -152,255 +304,14 @@ int main (int argc, char* argv[])
 			ptr = strchr(buffer,'\n'); // Or lf if no comments
 		if (ptr)
 			*ptr = '\0';
-		switch (next_token(&val,lex,buffer,&ptr))
-		{
-		case NONE:
-			continue;
-		case WORD:
-			break;
-		default:
-			fprintf(stderr,"%s:%d: error: OP expected\n",infile,line);
-			continue;
-		}
-		if (!strcmp(lex,"SAVE"))
-		{
-			if (next_token(&val,lex,ptr,&ptr) != PUNC || *lex != '[')
-			{
-				fprintf(stderr,"%s:%d: error: Expected '[' after SAVE\n",infile,line);
-				continue;
-			}
-			if (next_token(&val,lex,ptr,&ptr) != INT)
-			{
-				fprintf(stderr,"%s:%d: error: Expected integer after 'SAVE['\n",infile,line);
-				continue;
-			}
-			instr |= ((val & 0xFF) << IMGADDR_OFF);
-			ctrl = SAVE;
-			if (next_token(&val,lex,ptr,&ptr) != PUNC || *lex != ']')
-			{
-				fprintf(stderr,"%s:%d: error: Expected ']'\n",infile,line);
-				continue;
-			}
-			if (next_token(&val,lex,ptr,&ptr) != PUNC || *lex != ',')
-			{
-				fprintf(stderr,"%s:%d: error: Expected ','\n",infile,line);
-				continue;
-			}
-		}
-		if (!strcmp(lex,"LOAD"))
-		{
-			if (next_token(&val,lex,ptr,&ptr) != PUNC || *lex != '[')
-			{
-				fprintf(stderr,"%s:%d: error: Expected '[' after LOAD\n",infile,line);
-				continue;
-			}
-			if (next_token(&val,lex,ptr,&ptr) != INT)
-			{
-				fprintf(stderr,"%s:%d: error: Expected integer after 'LOAD['\n",infile,line);
-				continue;
-			}
-			instr |= ((val & 0xFF) << IMGADDR_OFF);
-			ctrl = SAVE;
-			if (next_token(&val,lex,ptr,&ptr) != PUNC || *lex != ']')
-			{
-				fprintf(stderr,"%s:%d: error: Expected ']'\n",infile,line);
-				continue;
-			}
-			if (next_token(&val,lex,ptr,&ptr) != PUNC || *lex != ',')
-			{
-				fprintf(stderr,"%s:%d: error: Expected ','\n",infile,line);
-				continue;
-			}
-		}
-		if (ptr)
-		{
-			while (ptr > buffer && isspace(ptr[-1])) --ptr; // Scan backward through trailing ws
-			*ptr = '\0'; // Terminate
-		}
-		ptr = buffer;
-		while (isspace(*ptr)) ++ptr;
-		++line;
-		if (*ptr == '\0') continue; // Ignore blank lines
-		instr = 0;
-		while (isspace(*ptr)) ++ptr;
-		peq = strchr(ptr,'=');
-		if (peq) // Parse LHS
-		{
-			char* pcom;
-			char* p = peq-1;
-			*peq++ = '\0'; // Terminate at '=';
-			while(p > ptr && isspace(*p)) *p = '\0'; // Rtrim
-			while(ptr)
-			{
-				pcom = strchr(ptr,','); // Look for a comma
-				if (pcom)
-				{
-					*pcom = '\0';
-					p = pcom-1;
-					while(p > ptr && isspace(*p)) *p = '\0'; // Rtrim
-					++pcom;
-				}
-				if (!strncmp(ptr,"RAM[",4)) // RAM assignment
-				{
-					int addr = strtoul(ptr+4,&ptr,0);
-					if (!ptr || *ptr++ != ']')
-					{
-						fprintf(stderr,"%s:%d: error: Expected ']' after RAM[\n",infile,line);
-						continue;
-					}
-					instr |= ((addr & 0xFF) << ADDR_OFF) | (1 << RAM_OFF);
-				}
-				else if (!strncmp(ptr,"IMG[",4))
-				{
-					int addr = strtoul(ptr+4,&ptr,0);
-					if (!ptr || *ptr++ != ']')
-					{
-						fprintf(stderr,"%s:%d: error: Expected ']' after IMG[\n",infile,line);
-						continue;
-					}
-					instr |= ((addr & 0xFF) << IMGADDR_OFF);
-					ctrl = SAVE;
-				}
-				else if (!strcmp(ptr,"FLAG")) // Flag assignment
-					instr |= 1 << FLAG_OFF;
-				else if (!strcmp(ptr,"NEWS")) // NEWS assignment
-					instr |= 1 << NEWS_OFF;
-				else if (!strcmp(ptr,"X")) // Etc...
-					instr |= 1 << GPREG_OFF;
-				else if (!strcmp(ptr,"Y"))
-					instr |= 2 << GPREG_OFF;
-				else if (!strcmp(ptr,"Z"))
-					instr |= 3 << GPREG_OFF;
-				else
-				{
-					fprintf(stderr,"%s:%d: error: Unknown destination '%s'\n",infile,line,ptr);
-					continue;
-				}
-				ptr = pcom;
-			}
-			ptr = peq;
-		}
-		while (isspace(*ptr)) ++ptr;
-		param = strchr(ptr,'('); // Operator;
-		if (param)
-			*param++ = '\0';
-		if (*ptr == '!')
-		{
-			instr |= (1 << INVOUT_OFF);
-			++ptr;
-		}
-		if (!strcmp(ptr,"LD"))
-			instr |= OP_LD << ALU_OFF; // No change really, but for consistency sake
-		else if (!strcmp(ptr,"AND"))
-			instr |= (OP_AND << ALU_OFF);
-		else if (!strcmp(ptr,"ANDINV"))
-			instr |= (OP_AND << ALU_OFF) | (1 << INVACC_OFF);
-		else if (!strcmp(ptr,"OR"))
-			instr |= (OP_OR << ALU_OFF);
-		else if (!strcmp(ptr,"ORINV"))
-			instr |= (OP_OR << ALU_OFF) | (1 << INVACC_OFF);
-		else if (!strcmp(ptr,"XOR"))
-			instr |= (OP_XOR << ALU_OFF);
-		else if (!strcmp(ptr,"XORINV"))
-			instr |= (OP_XOR << ALU_OFF) | (1 << INVACC_OFF);
-		else if (!strcmp(ptr,"XOR"))
-			instr |= (OP_XOR << ALU_OFF);
-		else if (!strcmp(ptr,"XORINV"))
-			instr |= (OP_XOR << ALU_OFF) | (1 << INVACC_OFF);
-		else if (!strcmp(ptr,"SUM"))
-			instr |= (OP_SUM << ALU_OFF) | (1 << CLRCAR_OFF);
-		else if (!strcmp(ptr,"SUMINV"))
-			instr |= (OP_SUM << ALU_OFF) | (1 << INVACC_OFF) | (1 << CLRCAR_OFF);
-		else if (!strcmp(ptr,"SUMC"))
-			instr |= (OP_SUM << ALU_OFF);
-		else if (!strcmp(ptr,"SUMCINV"))
-			instr |= (OP_SUM << ALU_OFF) | (1 << INVACC_OFF);
-		else if (!strcmp(ptr,"1") || !strcmp(ptr,"SET"))
-			instr |= (OP_SET << ALU_OFF), paramreq = 0;
-		else if (!strcmp(ptr,"0") || !strcmp(ptr,"CLR"))
-			instr |= (OP_CLR << ALU_OFF), paramreq = 0;
-		else if (!strcmp(ptr,"LDCARRY"))
-			instr |= (OP_CAR << ALU_OFF) | (1 << CLRCAR_OFF), paramreq = 0;
-		else
-		{
-			fprintf(stderr,"%s:%d: error: Invalid operation: '%s'\n",infile,line,ptr);
-			continue;
-		}
-		if (paramreq && !param)
-		{
-			fprintf(stderr,"%s:%d: error: Missing parameter\n",infile,line);
-			continue;
-		}
-		if (param)
-		{
-			paramend = strchr(param,')');
-			if (!paramend)
-			{
-				fprintf(stderr,"%s:%d: error: Expected: ')'\n",infile,line);
-				continue;
-			}
-			*paramend = '\0';
-			if (!strncmp(param,"RAM[",4))
-			{
-				if (instr & (1 << RAM_OFF))
-				{
-					fprintf(stderr,"%s:%d: error: Ram cannot be read and written in same instruction\n",infile,line);
-					continue;
-				}
-				int addr = strtoul(param+4,&ptr,0);
-				if (!ptr || *ptr++ != ']')
-				{
-					fprintf(stderr,"%s:%d: error: Expected ']' after RAM[\n",infile,line);
-					continue;
-				}
-				instr |= ((addr & 0xFF) << ADDR_OFF) | (IN_RAM << INSEL_OFF);
-			}
-			else if (!strncmp(param,"IMG[",4))
-			{
-				int addr = strtoul(param+5,&ptr,0);
-				if (!param || *ptr++ != ']')
-				{
-					fprintf(stderr,"%s:%d: error: Expected ']' after IMG[\n",infile,line);
-					continue;
-				}
-				instr |= ((addr & 0xFF) << IMGADDR_OFF) | (IN_WEST << INSEL_OFF);
-				ctrl = LOAD;
-			}
-			else if (!strcmp(param,"NORTH"))
-				instr |= (IN_NORTH << INSEL_OFF);
-			else if (!strcmp(param,"EAST"))
-				instr |= (IN_EAST << INSEL_OFF);
-			else if (!strcmp(param,"SOUTH"))
-				instr |= (IN_SOUTH << INSEL_OFF);
-			else if (!strcmp(param,"WEST"))
-				instr |= (IN_WEST << INSEL_OFF);
-			else if (!strcmp(param,"X"))
-				instr |= (IN_X << INSEL_OFF);
-			else if (!strcmp(param,"Y"))
-				instr |= (IN_Y << INSEL_OFF);
-			else if (!strcmp(param,"Z"))
-				instr |= (IN_Z << INSEL_OFF);
-			else
-			{
-				fprintf(stderr,"%s:%d: error: Invalid input '%s'\n",infile,line,param);
-				continue;
-			}
-		}
-		instr |= ctrl << CTRL_OFF;
+		++lineno;
+		lexer_ptr = buffer;
 		
-		// Because of endianness, we print out byte-by-byte
-		fputc((instr>>0)&0xFF,out);
-		fputc((instr>>8)&0xFF,out);
-		fputc((instr>>16)&0xFF,out);
-		fputc((instr>>24)&0xFF,out);
+		parse_instr(out);
 	}
 	// Add HALT instruction
 	instr = (HALT << CTRL_OFF);
-	// Because of endianness, we print out byte-by-byte
-	fputc((instr>>0)&0xFF,out);
-	fputc((instr>>8)&0xFF,out);
-	fputc((instr>>16)&0xFF,out);
-	fputc((instr>>24)&0xFF,out);
+	write_instr(out,instr);
 	fclose(out);
 	fclose(in);
 	return 0;
